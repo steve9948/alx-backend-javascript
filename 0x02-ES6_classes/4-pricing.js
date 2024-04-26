141c1d9601709e816e38fd6ef18b576797f7fa2a
steve998 <mbuguagichimu@gmail.com>
2024-04-26 17:48:19 +0000

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') throw TypeError('Amount must be a number');
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
