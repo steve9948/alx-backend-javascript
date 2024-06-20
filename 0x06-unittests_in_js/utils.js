895eec0de4b43c35df7ee87b9df5f515dc3aa759
steve9948 <mbuguagichimu@gmail.com>
2024-06-20 20:24:42 +0000

    Using spies

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,20 @@
+
+const Utils = {
+  calculateNumber(type, a, b) {
+    a = Math.round(a);
+    b = Math.round(b);
+
+    switch (type) {
+      case 'SUM':
+        return a + b;
+      case 'SUBTRACT':
+        return a - b;
+      case 'DIVIDE':
+        return b !== 0 ? a / b : 'Error';
+      default:
+        return 'Error';
+    }
+  }
+};
+
+module.exports = Utils;
